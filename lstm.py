a9ba79df8dec9eb43a89b59e7b31328b8d84e02f
Longxiao Zhang <zhanglongx@gmail.com>
2018-08-06 19:27:04 +0800

    lstm.py: add predict, refine with pylint

--- lstm.py
+++ lstm.py
@@ -26,7 +26,7 @@ def __read_data(mode='train', debug=False):
     ori_daily.fillna(method='ffill', axis=1, inplace=True)
 
     mean = ori_daily.mean()
-    
+
     percent = ori_daily.iloc[-1] / mean
     percent = percent > 1.2
 
@@ -34,7 +34,7 @@ def __read_data(mode='train', debug=False):
 
     labels = list(percent.index)
 
-    if debug == True:
+    if debug is True:
         percent.to_csv('t_labels.csv', encoding='gb2312')
 
     # X
@@ -50,7 +50,7 @@ def __read_data(mode='train', debug=False):
 
     reports = reports.stack(level=-1).swaplevel(axis=0)
 
-    if debug == True:
+    if debug is True:
         reports.to_csv('t_x.csv', encoding='gb2312')
 
     for (i, sym) in enumerate(labels):
@@ -141,68 +141,96 @@ class lstm():
         with tf.Session(graph=graph) as sess:
             sess.run(tf.global_variables_initializer())
             iteration = 1
-            
+
             for e in range(epochs):
-                # Initialize 
+                # Initialize
                 state = sess.run(initial_state)
-                
+
                 # Loop over batches
                 for x,y in self._get_batches(X_tr, y_tr):
-                    
+
                     # Feed dictionary
-                    feed = {inputs_ : x, labels_ : y, keep_prob_ : 0.5, 
+                    feed = {inputs_ : x, labels_ : y, keep_prob_ : 0.5,
                             initial_state : state, learning_rate_ : learning_rate}
-                    
-                    loss, _ , state, acc = sess.run([cost, optimizer, final_state, accuracy], 
+
+                    loss, _ , state, acc = sess.run([cost, optimizer, final_state, accuracy], \
                                                      feed_dict = feed)
                     train_acc.append(acc)
                     train_loss.append(loss)
-                    
+
                     # Print at each 5 iters
-                    if (iteration % 5 == 0):
+                    if iteration % 5 == 0:
                         print("Epoch: {}/{}".format(e, epochs),
                               "Iteration: {:d}".format(iteration),
                               "Train loss: {:6f}".format(loss),
                               "Train acc: {:.6f}".format(acc))
-                    
+
                     # Compute validation loss at every 25 iterations
-                    if (iteration%25 == 0):
-                        
+                    if iteration % 25 == 0:
+
                         # Initiate for validation set
                         val_state = sess.run(cell.zero_state(batch_size, tf.float32))
-                        
+
                         val_acc_ = []
                         val_loss_ = []
                         for x_v, y_v in self._get_batches(X_vld, y_vld):
                             # Feed
                             feed = {inputs_ : x_v, labels_ : y_v, keep_prob_ : 1.0, initial_state : val_state}
-                            
+
                             # Loss
-                            loss_v, state_v, acc_v = sess.run([cost, final_state, accuracy], feed_dict = feed)
-                            
+                            loss_v, _, acc_v = sess.run([cost, final_state, accuracy], feed_dict = feed)
+
                             val_acc_.append(acc_v)
                             val_loss_.append(loss_v)
-                        
+
                         # Print info
                         print("Epoch: {}/{}".format(e, epochs),
                               "Iteration: {:d}".format(iteration),
                               "Validation loss: {:6f}".format(np.mean(val_loss_)),
                               "Validation acc: {:.6f}".format(np.mean(val_acc_)))
-                        
+
                         # Store
                         validation_acc.append(np.mean(val_acc_))
                         validation_loss.append(np.mean(val_loss_))
-                    
-                    # Iterate 
+
+                    # Iterate
                     iteration += 1
-            
+
             saver.save(sess,"checkpoints/lstm.ckpt")
+
+    def predict(self, x_in, y_in):
+
+        y_test = self._one_hot(y_in)
+
+        inputs_    = self.inputs_
+        labels_    = self.labels_
+        keep_prob_ = self.keep_prob_
+
+        graph = self.graph
+
+        with graph.as_default():
+            saver = tf.train.Saver()
+
+        accuracy = self.accuracy
+
+        test_acc = []
+
+        with tf.Session(graph=graph) as sess:
+            # Restore
+            saver.restore(sess, tf.train.latest_checkpoint('checkpoints'))
+
+            for x_t, y_t in self._get_batches(x_in, y_test):
+                feed = {inputs_: x_t,
+                        labels_: y_t,
+                        keep_prob_: 1}
+
+                batch_acc = sess.run(accuracy, feed_dict=feed)
+                test_acc.append(batch_acc)
+            print("Test accuracy: {:.6f}".format(np.mean(test_acc)))
 
     @staticmethod
     def _standardize(train):
         """ Standardize data """
-
-        x_mean = np.mean(train, axis=0)
 
         # Standardize train and test
         X_train = (train - np.mean(train, axis=0)[None,:,:]) / np.std(train, axis=0)[None,:,:]
@@ -236,7 +264,7 @@ class lstm():
         #### Hyperparameters
         lstm_size     = 3*n_channels  # 3 times the amount of channels
         lstm_layers   = 2             # Number of layers
-        seq_len       = n_steps       # Number of steps # tempz
+        seq_len       = n_steps       # Number of steps
 
         graph = tf.Graph()
 
@@ -248,8 +276,8 @@ class lstm():
             learning_rate_ = tf.placeholder(tf.float32, name = 'learning_rate')
 
         # Build Convolutional Layer(s)
-        # 
-        # Questions: 
+        #
+        # Questions:
         # * Should we use a different activation? Like tf.nn.tanh?
         # * Should we use pooling? average or max?
         # In[9]:
@@ -257,7 +285,7 @@ class lstm():
         # Convolutional layers
         with graph.as_default():
             # (batch, samples, n_channels) --> (batch, samples, n_channels * 2)
-            conv1 = tf.layers.conv1d(inputs=inputs_, filters=n_channels * 2, kernel_size=2, strides=1, 
+            conv1 = tf.layers.conv1d(inputs=inputs_, filters=n_channels * 2, kernel_size=2, strides=1, \
                                      padding='same', activation = tf.nn.relu)
             n_ch = n_channels * 2
 
@@ -268,40 +296,40 @@ class lstm():
             # Construct the LSTM inputs and LSTM cells
             lstm_in = tf.transpose(conv1, [1,0,2])    # reshape into (seq_len, batch, channels)
             lstm_in = tf.reshape(lstm_in, [-1, n_ch]) # Now (seq_len*N, n_channels)
-            
+
             # To cells
             lstm_in = tf.layers.dense(lstm_in, lstm_size, activation=None) # or tf.nn.relu, tf.nn.sigmoid, tf.nn.tanh?
-            
+
             # Open up the tensor into a list of seq_len pieces
             lstm_in = tf.split(lstm_in, seq_len, 0)
-            
+
             # Add LSTM layers
-            lstm = tf.contrib.rnn.BasicLSTMCell(lstm_size)
-            drop = tf.contrib.rnn.DropoutWrapper(lstm, output_keep_prob=keep_prob_)
-            cell = tf.contrib.rnn.MultiRNNCell([drop] * lstm_layers)
+            lstm_cell = tf.contrib.rnn.BasicLSTMCell(lstm_size)
+            drop      = tf.contrib.rnn.DropoutWrapper(lstm_cell, output_keep_prob=keep_prob_)
+            cell      = tf.contrib.rnn.MultiRNNCell([drop] * lstm_layers)
             initial_state = cell.zero_state(n_batches, tf.float32)
-        
+
         # Define forward pass and cost function:
         # In[11]:
 
         with graph.as_default():
             outputs, final_state = tf.contrib.rnn.static_rnn(cell, lstm_in, dtype=tf.float32,
                                                              initial_state = initial_state)
-            
+
             # We only need the last output tensor to pass into a classifier
             logits = tf.layers.dense(outputs[-1], n_classes, name='logits')
-            
+
             # Cost function and optimizer
             cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=logits, labels=labels_))
             #optimizer = tf.train.AdamOptimizer(learning_rate_).minimize(cost) # No grad clipping
-            
+
             # Grad clipping
             train_op = tf.train.AdamOptimizer(learning_rate_)
 
             gradients = train_op.compute_gradients(cost)
             capped_gradients = [(tf.clip_by_value(grad, -1., 1.), var) for grad, var in gradients]
             optimizer = train_op.apply_gradients(capped_gradients)
-            
+
             # Accuracy
             correct_pred = tf.equal(tf.argmax(logits, 1), tf.argmax(labels_, 1))
             accuracy = tf.reduce_mean(tf.cast(correct_pred, tf.float32), name='accuracy')
@@ -311,6 +339,7 @@ class lstm():
         self.keep_prob_     = keep_prob_
         self.learning_rate_ = learning_rate_
         self.cell           = cell
+        self.logits         = logits
 
         self.graph = graph
         self.cost  = cost
@@ -319,44 +348,21 @@ class lstm():
         self.optimizer     = optimizer
         self.accuracy      = accuracy
 
-def __lstm_test(X, Y, batch_size=100):
-
-    y_vld = self._one_hot(Y)
-
-    # TODO:
-    graph = tf.Graph()
-    with graph.as_default():
-        saver = tf.train.Saver()
-
-    test_acc = []
-
-    with tf.Session(graph=graph) as sess:
-        # Restore
-        saver.restore(sess, tf.train.latest_checkpoint('checkpoints'))
-        
-        for x_t, y_t in self._get_batches(X, y_vld, batch_size):
-            feed = {inputs_: x_t,
-                    labels_: y_t,
-                    keep_prob_: 1}
-            
-            batch_acc = sess.run(accuracy, feed_dict=feed)
-            test_acc.append(batch_acc)
-        print("Test accuracy: {:.6f}".format(np.mean(test_acc)))
-
 def main(mode='train'):
 
+    l = lstm(n_steps=4, n_channels=2, n_classes=2, n_batches=100)
+
     if mode == 'train':
-        (x, y, len) = __read_fake_data(n_samples=10000, n_steps=4)
+        (x, y, _) = __read_fake_data(n_samples=10000, n_steps=4)
         # (x, y, len) = __read_data(mode='train', debug=True)
 
-        l = lstm(n_steps=4, n_channels=2, n_classes=2, n_batches=100)
         l.train(x_in=x, y_in=y, epochs=100)
 
     elif mode == 'test':
-        (x, y, len) = __read_fake_data(n_samples=1000, n_steps=4)
+        (x, y, _) = __read_fake_data(n_samples=1000, n_steps=4)
         # (x, y, len) = __read_data(mode='test', debug=True)
 
-        __lstm_test(X=x, Y=y, batch_size=100)
+        l.predict(x_in=x, y_in=y)
 
 if __name__ == '__main__':
-    main(mode='train')
+    main(mode='test')
