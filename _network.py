8268feac2a957fc143439b321c8da622dd5074ab
Longxiao Zhang <zhanglongx@gmail.com>
2019-03-27 11:48:57 +0800

    _network.py: timeout -> 30

--- _network.py
+++ _network.py
@@ -37,7 +37,7 @@ def _get_one_url(url, retries=-1, encoding='utf-8'):
     # FIXME: support retries 
     while(1):
         try:
-            resp = requests.get(url=url, headers=headers, timeout=60)
+            resp = requests.get(url=url, headers=headers, timeout=30)
         except:
             continue
 
