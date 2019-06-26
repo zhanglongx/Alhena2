5f6e2e594d06d425cdcfdf19f7c5f267fe2cdf98
Longxiao Zhang <zhanglongx@gmail.com>
2019-06-26 13:15:08 +0800

    querier: modify CLI

--- querier.py
+++ querier.py
@@ -12,30 +12,26 @@ def main():
     parser.add_argument('-c', '--csv', dest='csv', action='store_true', help='csv output')
     parser.add_argument('-f', '--formula', type=str, nargs='?', help='formula or file input (.json)')
     parser.add_argument('-p', '--path', default='.', type=str, nargs='?', help='Alhena2 path')
-    parser.add_argument('-s', '--start', default='2013-01-01', type=str, help='start date')
+    parser.add_argument('-q', '--quarter', default=None, type=str, nargs='?', \
+                        help="quarter in ['Mar', 'Jun', 'Sep', 'Dec']")
+    parser.add_argument('-s', '--start', default='2000-01-01', type=str, help='start date')
     parser.add_argument('--disable-TTM', dest='TTM', default=True, action='store_false', help='TTM off')
-    parser.add_argument('symbols', default=None, type=str, nargs='*', help='symbols to extract')
+    parser.add_argument('key', default=None, type=str, nargs='?', help='key to extract')
 
     _formula = parser.parse_args().formula
     _path    = parser.parse_args().path
+    _quarter = parser.parse_args().quarter
     _start   = parser.parse_args().start
     _TTM     = parser.parse_args().TTM
-    _symbols = parser.parse_args().symbols
+    _key     = parser.parse_args().key
 
     if not _formula is None and os.path.exists(_formula):
         with open(_formula, encoding='utf-8') as f:
             _formula = json.load(f)
 
-    if len(_symbols) == 0:
-        _symbols = None
-    elif len(_symbols) == 1:
-        _symbols = _symbols[0]
-    else:
-        pass
+    _symbols = cn_info(path=_path).get(key=_key)
 
-    _symbols = cn_info(path=_path).get(key=_symbols)
-
-    report = cn_report(path=_path, symbols=_symbols, start=_start, TTM=_TTM, quarter=None, \
+    report = cn_report(path=_path, symbols=_symbols, start=_start, TTM=_TTM, quarter=_quarter, \
                        language='CN')
     df = report.get(formulas=_formula)
 
