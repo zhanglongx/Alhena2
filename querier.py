d2c5f8bd8397ec3cfa167a239174dbfc0c84b576
Longxiao Zhang <zhanglongx@gmail.com>
2019-07-15 15:33:04 +0800

    querier: add PEG, option to drop symbol when len == 1

--- querier.py
+++ querier.py
@@ -3,6 +3,9 @@
 import os
 import json
 import argparse
+import numpy as np
+
+import matplotlib.pyplot as plt
 
 from cn.cn_querier import (cn_info, cn_report)
 
@@ -10,6 +13,7 @@ def main():
     parser = argparse.ArgumentParser(description='''wrapper for Alhena2 querier''')
 
     parser.add_argument('-c', '--csv', dest='csv', action='store_true', help='csv output')
+    parser.add_argument('-d', '--drop', dest='drop', action='store_true', help='drop symbol if only one')
     parser.add_argument('-f', '--formula', type=str, nargs='?', help='formula or file input (.json)')
     parser.add_argument('-p', '--path', default='.', type=str, nargs='?', help='Alhena2 path')
     parser.add_argument('-q', '--quarter', default=None, type=str, nargs='?', \
@@ -18,6 +22,7 @@ def main():
     parser.add_argument('--disable-TTM', dest='TTM', default=True, action='store_false', help='TTM off')
     parser.add_argument('key', default=None, type=str, nargs='?', help='key to extract')
 
+    _drop    = parser.parse_args().drop
     _formula = parser.parse_args().formula
     _path    = parser.parse_args().path
     _quarter = parser.parse_args().quarter
@@ -34,6 +39,16 @@ def main():
     report = cn_report(path=_path, symbols=_symbols, start=_start, TTM=_TTM, quarter=_quarter, \
                        language='CN')
     df = report.get(formulas=_formula)
+
+    if len(df.index.levels[0]) == 1 and _drop:
+        df.index = df.index.droplevel(0)
+
+    def __PEG(df):
+        df['p_pct'] = df.groupby(level=0)['profit'].apply(lambda x: x.pct_change())
+        df.loc[df['p_pct'] < 0, 'p_pct'] = np.NaN
+        df['PEG'] = df['PE'] / df['p_pct']
+
+    __PEG(df)
 
     df.to_csv('t.csv')
 
